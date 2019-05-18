7a126e559a01649309ccaeaa5422207e2f21cda3
Jayde Fraanz <216172489@mycput.ac.za>
2019-05-18 20:56:58 +0200

    complete test file 1

--- src/main/java/za/ac/cput/repositories/implimentation/BusRepositoryImp/BusDetailRepositoryImpl.java
+++ src/main/java/za/ac/cput/repositories/implimentation/BusRepositoryImp/BusDetailRepositoryImpl.java
@@ -4,12 +4,13 @@ import za.ac.cput.Domain.Bus.BusDetails;
 import za.ac.cput.repositories.BusRepository.BusDetailsRepository;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class BusDetailRepositoryImpl implements BusDetailsRepository {
 
     private static BusDetailsRepository repository = null;
-    private Set<BusDetails> busDetails;
+    public Set<BusDetails> busDetails;
 
     public BusDetailRepositoryImpl(){this.busDetails = new HashSet<>(); }
 
@@ -35,15 +36,23 @@ public class BusDetailRepositoryImpl implements BusDetailsRepository {
     @Override
     public BusDetails update(BusDetails busDetails)
     {
-        for (int i = 0; i < this.busDetails.size(); i++)
+        Iterator<BusDetails> it = this.busDetails.iterator();
+
+        while(it.hasNext())
         {
-                  if(this.busDetails.contains(busDetails.getBusNo()))
-                  {
-                      //remove the current details
-                      this.busDetails.remove(busDetails.getBusNo());
-                      //add the new information
-                      this.busDetails.add(busDetails);
-                  }
+           if(this.busDetails.contains(busDetails))
+           {
+               System.out.println("updating details");
+               this.busDetails.remove(busDetails);
+               this.busDetails.add(busDetails);
+               break;
+           }
+           else
+           {
+               System.out.println("adding new details");
+               this.busDetails.add(busDetails);
+           }
+           it.next();
         }
         return busDetails;
     }
@@ -51,22 +60,27 @@ public class BusDetailRepositoryImpl implements BusDetailsRepository {
     @Override
     public void delete(String s)
     {
-        this.busDetails.remove(s);
+        for (BusDetails busDtail: busDetails)
+        {
+            if(busDtail.getBusNo() == s)
+            {
+                busDetails.remove(busDtail);
+            }
+        }
     }
 
     @Override
     public BusDetails read(String s)
     {
-        for (int i = 0; i < this.busDetails.size(); i++)
+        BusDetails bdts = new BusDetails.Builder().build();
+        for (BusDetails busDtail: busDetails)
         {
-
-            if (this.busDetails.contains(s))
+            if(busDtail.getBusNo() == s)
             {
-                System.out.println("found");
-                System.out.println(busDetails.toString());
+               bdts = busDtail;
             }
         }
 
-        return read(s);
+        return bdts;
     }
 }
